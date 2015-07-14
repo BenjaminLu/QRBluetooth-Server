0f45d9c1bd8868b84452c8db00540f3e46f011e1
BenjaminLu <coopermilk123@gmail.com>
2015-07-15 03:34:40 +0800

    fix bug: exit activity when AlertDialog been clicked.

--- app/src/main/java/com/bluetooth/icollect/bluetooth/MainActivity.java
+++ app/src/main/java/com/bluetooth/icollect/bluetooth/MainActivity.java
@@ -1,17 +1,19 @@
 package com.bluetooth.icollect.bluetooth;
 
-import android.app.Activity;
 import android.bluetooth.BluetoothAdapter;
 import android.bluetooth.BluetoothDevice;
 import android.bluetooth.BluetoothServerSocket;
 import android.bluetooth.BluetoothSocket;
 import android.content.BroadcastReceiver;
 import android.content.Context;
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.content.IntentFilter;
 import android.graphics.Bitmap;
 import android.os.Bundle;
 import android.os.Message;
+import android.support.v7.app.AlertDialog;
+import android.support.v7.app.AppCompatActivity;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.widget.ArrayAdapter;
@@ -31,7 +33,7 @@ import android.os.Handler;
 import net.glxn.qrgen.android.QRCode;
 
 
-public class MainActivity extends Activity
+public class MainActivity extends AppCompatActivity
 {
     private static final int REQUEST_ENABLE_BT = 1;
     private static final int MESSAGE_READ = 2;
@@ -45,15 +47,27 @@ public class MainActivity extends Activity
     String deviceAddress;
     private ImageView imageView;
     ConnectedThread connectedThread;
+    private BluetoothSocket mmSocket;
+    private InputStream mmInStream;
+    private OutputStream mmOutStream;
+    AlertDialog.Builder alertDialog;
     private final Handler mHandler = new Handler()
     {
         public void handleMessage(Message msg)
         {
             String message = msg.getData().getString("message");
             statusTextView.setText(message);
+           showDialog();
         }
     };
 
+    private void showDialog()
+    {
+        if(!(this.isFinishing()))
+        {
+            alertDialog.show();
+        }
+    }
     private final BroadcastReceiver mReceiver = new BroadcastReceiver()
     {
         @Override
@@ -110,6 +124,26 @@ public class MainActivity extends Activity
     {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        alertDialog = new AlertDialog.Builder(MainActivity.this).setTitle("確認訊息")
+                .setMessage("確定為消費者增加點數?").setPositiveButton("好", new DialogInterface.OnClickListener()
+        {
+            @Override
+            public void onClick(DialogInterface dialog, int which)
+            {
+                //response
+                String response = "Sent from " + deviceName;
+                try {
+                    mmOutStream.write(response.getBytes());
+                    mmSocket.close();
+                    finish();
+                    System.exit(0);
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+        });
+
         arrayAdapter = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1);
         statusTextView = (TextView) findViewById(R.id.status);
         imageView = (ImageView) findViewById(R.id.imageView);
@@ -197,20 +231,18 @@ public class MainActivity extends Activity
 
     private class AcceptThread extends Thread
     {
-        private final BluetoothServerSocket mmServerSocket;
+        private BluetoothServerSocket mmServerSocket;
 
         public AcceptThread()
         {
             // Use a temporary object that is later assigned to mmServerSocket,
             // because mmServerSocket is final
-            BluetoothServerSocket tmp = null;
             try {
                 // MY_UUID is the app's UUID string, also used by the client code
-                tmp = bluetoothAdapter.listenUsingRfcommWithServiceRecord(deviceName, uuid);
+                mmServerSocket = bluetoothAdapter.listenUsingRfcommWithServiceRecord(deviceName, uuid);
             } catch (IOException e) {
                 e.printStackTrace();
             }
-            mmServerSocket = tmp;
         }
 
         public void run()
@@ -219,9 +251,11 @@ public class MainActivity extends Activity
             while (true) {
                 BluetoothSocket socket = null;
                 try {
-                    socket = mmServerSocket.accept();
-                    connectedThread = new ConnectedThread(socket);
-                    connectedThread.start();
+                    if(mmServerSocket != null) {
+                        socket = mmServerSocket.accept();
+                        connectedThread = new ConnectedThread(socket);
+                        connectedThread.start();
+                    }
                 } catch (IOException e) {
                     e.printStackTrace();
                     break;
@@ -245,15 +279,9 @@ public class MainActivity extends Activity
 
     private class ConnectedThread extends Thread
     {
-        private final BluetoothSocket mmSocket;
-        private final InputStream mmInStream;
-        private final OutputStream mmOutStream;
-
         public ConnectedThread(BluetoothSocket socket)
         {
             mmSocket = socket;
-            InputStream tmpIn = null;
-            OutputStream tmpOut = null;
             mHandler.post(new Runnable()
             {
                 @Override
@@ -266,55 +294,36 @@ public class MainActivity extends Activity
             // Get the input and output streams, using temp objects because
             // member streams are final
             try {
-                tmpIn = socket.getInputStream();
-                tmpOut = socket.getOutputStream();
+                mmInStream = socket.getInputStream();
+                mmOutStream = socket.getOutputStream();
             } catch (IOException e) {
                 e.printStackTrace();
             }
-
-            mmInStream = tmpIn;
-            mmOutStream = tmpOut;
         }
 
         public void run()
         {
             byte[] buffer = new byte[4096];  // buffer store for the stream
-            StringBuffer sb = new StringBuffer();
-            StringBuilder stringBuilder = new StringBuilder();
             int bytes; // bytes returned from read()
 
-            // Keep listening to the InputStream until an exception occurs
-            while (true) {
+            // Read from the InputStream
+            if(mmSocket.isConnected()) {
                 try {
-                    // Read from the InputStream
-                    if(mmSocket.isConnected()) {
-                        bytes = mmInStream.read(buffer);
-                        // Send the obtained bytes to the UI activity
-                        final String message = new String(buffer, 0, bytes);
-                        mHandler.post(new Runnable()
-                        {
-                            @Override
-                            public void run()
-                            {
-                                statusTextView.setText(message);
-                            }
-                        });
-                        Message m = new Message();
-                        Bundle b = new Bundle();
-                        b.putString("message", message);
-                        m.setData(b);
-                        mHandler.sendMessage(m);
-
-                        //response
-                        String response = "Sent from " + deviceName;
-                        connectedThread.write(response.getBytes());
-                        mmSocket.close();
-                    } else {
-                        break;
-                    }
+                    bytes = mmInStream.read(buffer);
+                    // Send the obtained bytes to the UI activity
+                    String message = new String(buffer, "UTF-8");
+                    Message m = new Message();
+                    Bundle b = new Bundle();
+                    b.putString("message", message);
+                    m.setData(b);
+                    mHandler.sendMessage(m);
                 } catch (IOException e) {
+                    try {
+                        mmSocket.close();
+                    } catch (IOException e1) {
+                        e1.printStackTrace();
+                    }
                     e.printStackTrace();
-                    break;
                 }
             }
         }
